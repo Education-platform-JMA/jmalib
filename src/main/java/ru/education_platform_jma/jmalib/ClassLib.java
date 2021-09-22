df25bdb2f5d763819ad03f7729bfc317fc42e0b1
Sergey Melnikov <74730932+pdasilem@users.noreply.github.com>
2021-09-22 11:58:38 +0500

    Add files via upload

---
+++ src/main/java/ru/education_platform_jma/jmalib/ClassLib.java
@@ -0,0 +1,9 @@
+package ru.jma.jmalib;
+
+// you should add needed functionality to the class (module)
+public class ClassLib {
+
+    public static void main(String[] args) {
+        System.out.println("it works!");
+    }
+}
